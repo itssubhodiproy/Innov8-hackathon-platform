d66ac31ae5fcc09d8fd708b46c8e48654e518690
subh-23 <subhodiproy281@gmail.com>
2023-03-24 12:02:37 +0530

    comment added to routes

--- routes/project/index.js
+++ routes/project/index.js
@@ -11,12 +11,16 @@ const { JWT_AUTH } = require("../../controller/config/jwtAuth");
 // create project (private route)
 router.post("/create-project", JWT_AUTH, CreateProject);
 
+// update project (private route)
 router.put("/update-project", JWT_AUTH, UpdateProject);
 
+// delete project (private route)
 router.delete("/delete-project", JWT_AUTH, DeleteProject);
 
+// get all projects of current loggedIn user (private route)
 router.get("/get-all-projects", JWT_AUTH, GetAllProjects);
 
+// get a single project by ID (private route)
 router.get("/get-project", JWT_AUTH, GetProjectById);
 
 module.exports = router;
